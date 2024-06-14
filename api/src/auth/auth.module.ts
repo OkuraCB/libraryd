7efd70bb064465a18fa52347c1fcc063e2739cca
arthur <arthur-illa@hotmail.com>
2024-06-13 22:19:27 -0300

    fixing bugsw

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,12 +1,12 @@
-import { LocalStrategy } from './strategies/local.strategy';
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from './strategies/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 import { PrismaService } from 'src/prisma.service';
+import { UsersModule } from 'src/users/users.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
--- api/src/auth/strategies/jwt.strategy.ts
+++ api/src/auth/strategies/jwt.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ExtractJwt } from 'passport-jwt';
+import { Strategy } from 'passport-local';
 
 export interface JwtPayload {
   sub: number;
